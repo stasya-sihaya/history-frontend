d6fed2e7dc6dca2c7da3d776cea78e8dba34a4ce
Stasya <st.klochok@gmail.com>
2016-04-09 19:17:44 +0000

    Add scafolding for breadcrumbs and menu in Shared

---
+++ src/app/app.js
@@ -0,0 +1,2 @@
+'use strict';
+var app = angular.module('app', ['ngRoute', 'ngAnimate', 'angularMoment', 'angular-preload-image', 'truncate', 'app.routes', 'app.core', 'app.services', 'app.config']);
